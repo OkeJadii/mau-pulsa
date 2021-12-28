9183b6610bb27ebbffcc3fccf6b5f241112dfc85
regaasadarma <regaasadarmq@gmail.com>
2021-12-28 21:15:21 +0800

    move modal to detail

--- resources/views/user/pulsa/detail.blade.php
+++ resources/views/user/pulsa/detail.blade.php
@@ -204,13 +204,45 @@
                   Total Pembayaran : 5000
                 </div>
                 <div class="card-body">
-                    <button type="submit" class="btn-beli border-0" style="width: 100%">BAYAR SEKARANG! <i class="fas fa-shopping-bag"></i></button>
+                    <button type="submit" data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="btn-beli border-0" style="width: 100%">
+                        BAYAR SEKARANG! <i class="fas fa-shopping-bag"></i>
+                    </button>
                 </div>
+
               </div>
         </div>
 
 
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+            <img src="/img/GoPay-Logo.png" alt="" style="width: 20%;">
+                <h3 class="bi bi-x-circle-fill" data-bs-dismiss="modal"></h3>
+            </div>
+            <div class="modal-body ">
+                <!-- <div class="text-right"> <i class="fa fa-close close" data-dismiss="modal"></i> </div> -->
+                <!-- <div class="px-4 py-5"> -->
+                <h5 class="text-center">Untuk menyelesaikan pembayaran dengan GO-PAY, buka aplikasi GO-JEK dan scan kode QR</h5>
+                    <img src="/img/qrcode.png" class="rounded mx-auto d-block" style="width: 60%;">
+                    <span class="theme-color">TOTAL</span>
+                    <div class="mb-3">
+                        <hr class="new1 font-weight-bold ">
+                    </div>
+                    <div class="d-flex justify-content-between"> <small>Nama Produk</small> <small>Pulsa Indosat</small> </div>
+                    <div class="d-flex justify-content-between"> <small>Harga</small> <small>Rp 10.000</small> </div>
+                    <div class="d-flex justify-content-between"> <small>Biaya Admin</small> <small>Rp 2.000</small> </div>
+                    <div class="d-flex justify-content-between mt-3"> <span class="font-weight-bold">Total</span> <span class="font-weight-bold theme-color">Rp. 12.000</span> </div>
+                    <div class="text-center mt-5"> <button class="btn btn-beli">Selesai</button> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+</div>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
     </div>
     </li>
     </ul>
